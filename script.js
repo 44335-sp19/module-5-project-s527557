8190e57a175b65c7ca0f6f4b9c78de78c5c801b2
Allie Wolf <46724434+s527557@users.noreply.github.com>
2019-04-11 12:32:59 -0500

    project 5

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-/* Module 5 Project Script File */
+
+<!DOCTYPE html>
+<html>
+<body>
+
+<p>Click the button to demonstrate the prompt box.</p>
+
+<button onclick="myFunction()">Try it</button>
+
+<p id="demo"></p>
+
+<script>
+function showQuestion() {
+  var question = prompt("What is an anonymous function?", "use anonymous functions to pass arguments to a function that is called by an event handler");
+  if (question != null) {
+    document.getElementById("demo").innerHTML =
+    question + "! is correct!";
+  }
+}
+</script>
+
+</body>
+</html>
